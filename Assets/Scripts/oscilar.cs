e96566ac4f27f0e1a3143be771b1d90889169dcc
vriera <vrieratorraca@gmail.com>
2022-04-08 00:46:14 -0300

    oscilar ahora orbita

--- Assets/Scripts/oscilar.cs
+++ Assets/Scripts/oscilar.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class oscilar : MonoBehaviour
 {
-    private float sin_pos = 0;
+    private float x_pos = 0;
+    private float y_pos = 0;
+    private float z_pos = 0;
+    private Vector3 inicialPos;
     public Transform centro;
-    // Update is called once per frame
+    private float xv , yv  , zv;
+    private float xa , ya , za;
+ 
+    void Start(){
+        Roll();
+        inicialPos = transform.position;
+    }
+       // Update is called once per frame
     void Update()
     {
-        float velocity = 0.1F;
-        float amplitud = 1;
-        sin_pos = Mathf.Sin((Time.deltaTime * velocity *Mathf.PI)) * amplitud;
-        transform.position = new Vector3(Time.deltaTime,
-                                         transform.position.y,
-                                        transform.position.z  + sin_pos );  
+        x_pos = Mathf.Sin((Time.time * xv *Mathf.PI)) * xa;
+        y_pos = Mathf.Sin((Time.time * yv *Mathf.PI)) * ya;
+        z_pos = Mathf.Sin((Time.time * zv *Mathf.PI)) * za;
+        transform.position = inicialPos + centro.position + new Vector3(x_pos,y_pos,z_pos);   
 
     }
+    
+    void Roll(){
+        float direction = Random.Range(-1.0f , 1.0f);
+        direction = direction / Mathf.Abs(direction);
+        xa = Random.Range(2 , 6) * direction;
+        xv = Random.Range(0.2f , 0.5f)/2;
+        direction = Random.Range(-1.0f , 1.0f);
+        direction = direction / Mathf.Abs(direction);
+        ya = Random.Range(2 , 6) * direction;
+        yv = Random.Range(0.2f , 0.5f)/2;
+        direction = Random.Range(-1.0f , 1.0f);
+        direction = direction / Mathf.Abs(direction);
+        za = Random.Range(2 , 6) * direction;
+        zv = Random.Range(0.2f , 0.5f)/2;
+    }
+    
 }
