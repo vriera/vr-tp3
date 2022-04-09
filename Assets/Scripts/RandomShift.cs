6e65e9fa1f81158eeabbeadd5292fd09e40355e4
vriera <vrieratorraca@gmail.com>
2022-04-08 21:57:41 -0300

    added textures

--- Assets/Scripts/RandomShift.cs
+++ Assets/Scripts/RandomShift.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace PathCreation {
+    //Clase encargada de generar fouriers aleatorias
     public class RandomShift : MonoBehaviour
         {
             int levels;
             float[] speeds;
             float decay;   
             public RandomShift(int l , float d , float speedAc , float minSpeed , float maxSpeed){
+                //Decay afectara cuanto peso entran los senos a medida que aumenta el i
                 decay = d;
                 levels = l;
                 speeds = new float[levels];
+                //cada velocidad corresponde a un seno, los cuales aumentan su frecuencia
                 speeds[0] = Random.Range( minSpeed  , maxSpeed );
                 for(int i = 1 ; i< levels ; i++ ){
                     minSpeed = minSpeed + (speedAc / i);
@@ -18,6 +21,8 @@ namespace PathCreation {
                     speeds[i] = Random.Range( minSpeed,maxSpeed );  
                 }
             }
+
+            //Calcula la serie de fourier
             public float GetShift(float time){
                 float shift = Mathf.Sin(Mathf.PI * 2 * speeds[0] *time ) ;
                 for(int i = 1 ; i < levels ; i++){
